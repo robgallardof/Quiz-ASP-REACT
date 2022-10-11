9b08d18e744bbccdf56e0d37884b5e59bdf47c25
RobGallardo <robgallardof@gmail.com>
2022-10-10 23:48:09 -0500

    Se agregan cambios del front y del backend, controladores que estaban en otro repositorio que fue depurado y movido a este repositorio

---
+++ QuizApi/QuizApi/Migrations/20221010144921_initial create.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizApi.Migrations
+{
+    public partial class initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Participants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Score = table.Column<int>(type: "int", nullable: false),
+                    TimeTaken = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuestionInWords = table.Column<string>(type: "nvarchar(250)", nullable: true),
+                    ImageName = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    OptionOne = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    OptionTwo = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    OptionThree = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    OptionFour = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Answer = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Participants");
+
+            migrationBuilder.DropTable(
+                name: "Questions");
+        }
+    }
+}
---
+++ QuizApi/QuizApi/Models/Question.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuizApi.Models
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string? QuestionInWords
+        {
+            get; set;
+        }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? ImageName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string OptionOne { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string OptionTwo { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string OptionThree { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string OptionFour { get; set; }
+
+        public int Answer { get; set; }
+    }
+}
